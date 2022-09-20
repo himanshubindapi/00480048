618e708910faa4bd4d3ee4b6ac70e27378ac5c7f
himanshubindapi <himanshu.k@bindapi.com>
2022-09-20 11:38:28 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>0048</title>
+</head>
+<body>
+    <div>
+        <form method="POST">
+            <input type="text" placeholder="First Name" name="fname"><br><br>
+            <input type="text" placeholder="Second Name" name="sname"><br><br>
+            <button type="submit" name="submit">send</button><br><br>
+        </form>
+        <div>
+            <h2>Hello
+                <?php
+                     echo $_POST["fname"];
+                     echo $_POST["sname"];
+                     echo "! Greetings from BindAPI.";
+                 ?>
+
+                      
+
+                      
+            </h2>
+            
+        </div>
+    </div>
+</body>
+</html>
